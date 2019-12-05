aaccb0d52f830977dd9c21e27c9aa61469dfb5c3
ramcn <cn.ramachandra@gmail.com>
2019-12-05 12:28:14 +0530

    algo for ppt

---
+++ algo.py
@@ -0,0 +1,16 @@
+def network(X, W):
+    istate = Convolution(X, W, winlen, fun=tanh)
+    ostate1 = GruModBck(X, W, istate)
+    ostate2 = GruModFwd(X, W, ostate1)
+    ostate3 = GruModBck(X, W, ostate2)
+    ostate4 = GruModFwd(X, W, ostate3)
+    ostate5 = GruModBck(X, W, ostate4)
+    GlobalNormFlipFlop(size, nbase)
+
+def GruModFwd(X, W, istate):
+    for (i=0;i<N;i++):
+       GruModStep()
+
+def GruModBck(X, W, istate):
+    for (i=N;i>0;i--):
+       GruModStep()
