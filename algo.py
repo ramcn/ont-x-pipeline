5dd615abd5d3fceb1096f3f7ae5e65627c31aa74
ramcn <cn.ramachandra@gmail.com>
2020-02-06 18:53:02 +0530

    merge second gemv

--- algo.py
+++ algo.py
@@ -1,27 +1,22 @@
 def Network(X, W):
     conv = Convolution(X, W)
-    X1 = feedforward_linear(conv,W1)
-    ostate1 = GruModBck(X1,sW1)
-    X2 = feedforward_linear(X1,W2,ostate1)
-    ostate2 = GruModFwd(X2,sW2)
-    X3 = feedforward_linear(X2, W3,ostate2)
-    ostate3 = GruModBck(X3,sW3)
-    X4 = feedforward_linear(X3,W4,ostate3)
-    ostate4 = GruModFwd(X4,sW4)
-    X5 = feedforward_linear(X4,W5,ostate4)
-    ostate5 = GruModBck(X5,sW5)
-    trans = GlobalNormFlipFlop(ostate5)
+    X1 = GruModBck(X,sW1,W1)
+    X2 = GruModFwd(X1,sW2,W2)
+    X3 = GruModBck(X2,sW3,W3)
+    X4 = GruModFwd(X3,sW4,W4)
+    X5 = GruModBck(X3,sW5,W5)
+    trans = GlobalNormFlipFlop(X5)
 
-def GruModFwd(X,sW ):
+def GruModFwd(X,sW,W):
     for (i=0;i<N;i=i+1):
-       GruModStep(W,X[i],ostate)
+       GruModStep(sW,W,X[i],ostate)
 
-def GruModBck(X, sW):
+def GruModBck(X,sW,W):
     for (i=N;i>0;i=i-1):
-       GruModStep(W,X[i],ostate)
+       GruModStep(sW,W,X[i],ostate)
 
-def GruModStep(W,istate,ostate):
-     cblas_sgemv(768,256,1.0,W,istate,1,1.0,Cout,1)
+def GruModStep(sW,W,istate,ostate):
+     cblas_sgemv(768,256,1.0,sW,istate,1,1.0,Cout,1)
 
      for ( i = 0; i < 256; i = i+1):
        Cout[i] = LOGISTICF(Cout[i]) // Update gate u(t) 
@@ -31,4 +26,7 @@ def GruModStep(W,istate,ostate):
        ostate[i] = (-1) * Cout[i] * Cout[i+size+size] 
 			   + Cout[i+size+size]  // O(t) part 2
        ostate[i] = Cout[i] * istate[i] + ostate[i]  // O(t) part 1
+
+     cblas_sgemv(766,256,1.0,W,ostate,1,1.0,XnextBuf+bias,1);
+
 
